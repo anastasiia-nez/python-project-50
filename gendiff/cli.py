c389e8b92e48cf15e804b7e2dabfecca6d8c7508
Anastasiia Nezgovorova <odabcrfake@gmail.com>
2023-12-12 16:42:18 +0400

    Fix parameters of positional arguments and delete redundunt argument.

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -6,8 +6,7 @@ def cli():
     prog='gendiff',  # noqa: E122
     description='Compares two configuration files and shows a difference.')
 
-    parser.add_argument('first_file', type=str, required=True)
-    parser.add_argument('second_file', type=str, required=True)
-    parser.add_argument('-h', '--help', type=str, required=False)
+    parser.add_argument('first_file', type=str)
+    parser.add_argument('second_file', type=str)
     args = parser.parse_args()
     print(args.filename, args.count, args.verbose)
