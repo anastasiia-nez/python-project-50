ad1396527c3b2ba92f768a2100b2c462a47e3aba
Anastasiia Nezgovorova <odabcrfake@gmail.com>
2024-04-05 19:29:27 +0400

    Refactored tests.

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -2,34 +2,21 @@ import subprocess
 from gendiff.gendiff import generate_diff
 
 
-FIRST_FILE_PATH = 'gendiff/json/file1.json'
-SECOND_FILE_PATH = 'gendiff/json/file2.json'
+FIRST_FILE_PATH_FOR_FLAT = 'tests/fixtures/json/file1.json'
+SECOND_FILE_PATH_FOR_FLAT = 'tests/fixtures/json/file2.json'
+HELP = 'tests/fixtures/cli_help.txt'
+flat_diff_result = 'tests/fixtures/flat_diff.txt'
 
 
 def test_help_message():
     command = subprocess.run(['gendiff', '-h'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     assert command.returncode == 0
-    assert """usage: gendiff [-h] [-f FORMAT] first_file second_file
-
-Compares two configuration files and shows a difference.
-
-positional arguments:
-  first_file
-  second_file
-
-options:
-  -h, --help            show this help message and exit
-  -f FORMAT, --format FORMAT
-                        set format of output
-""" in command.stdout
+    with open(HELP) as file:
+        expected_result = file.read()
+    assert command.stdout == expected_result
 
 
 def test_generate_diff():
-    assert generate_diff(FIRST_FILE_PATH, SECOND_FILE_PATH) == '''{
-- follow: False
-  host: hexlet.io
-- proxy: 123.234.53.22
-- timeout: 50
-+ timeout: 20
-+ verbose: True
-}'''
+    with open(flat_diff_result) as file:
+        expected_result = file.read()
+    assert generate_diff(FIRST_FILE_PATH_FOR_FLAT, SECOND_FILE_PATH_FOR_FLAT) == expected_result
