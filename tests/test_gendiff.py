69a1964b8a98993b77cd84a33b845208ac6a96eb
Anastasiia Nezgovorova <odabcrfake@gmail.com>
2024-04-25 14:08:45 +0400

    Refactored tests and fixtures.

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,7 +5,7 @@ from gendiff.gendiff import generate_diff
 FIRST_FILE_PATH_FOR_FLAT = 'tests/fixtures/json/file1.json'
 SECOND_FILE_PATH_FOR_FLAT = 'tests/fixtures/json/file2.json'
 HELP = 'tests/fixtures/cli_help.txt'
-flat_diff_result = 'tests/fixtures/flat_diff.txt'
+flat_diff_json_1_result = 'tests/fixtures/flat_diff_json_1.txt'
 
 
 def test_help_message():
@@ -13,10 +13,12 @@ def test_help_message():
     assert command.returncode == 0
     with open(HELP) as file:
         expected_result = file.read()
-    assert command.stdout == expected_result
+        assert command.stdout == expected_result
 
 
 def test_generate_diff():
-    with open(flat_diff_result) as file:
+    with open(flat_diff_json_1_result) as file:
         expected_result = file.read()
-    assert generate_diff(FIRST_FILE_PATH_FOR_FLAT, SECOND_FILE_PATH_FOR_FLAT) == expected_result
+        assert generate_diff(FIRST_FILE_PATH_FOR_FLAT, SECOND_FILE_PATH_FOR_FLAT) == expected_result
+
+    
